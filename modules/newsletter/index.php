49434df06c465960a1607f5c03ed5868969279da
Luca Realdi <luca.realdi@opencontent.it>
2025-05-08 16:02:41 +0200

    Remove breadcrumb from newsletter/index

--- modules/newsletter/index.php
+++ modules/newsletter/index.php
@@ -33,4 +33,17 @@ $Result['path'] = array( array( 'url'  => false,
                          array( 'url'  => false,
                                 'text' => ezpI18n::tr( 'cjw_newsletter/index', 'Dashboard' ) ) );
 
-?>
+$contentInfoArray = [
+    'node_id' => null,
+    'class_identifier' => null,
+];
+$contentInfoArray['persistent_variable'] = [
+    'show_path' => false,
+];
+if (is_array($tpl->variable('persistent_variable'))) {
+    $contentInfoArray['persistent_variable'] = array_merge(
+        $contentInfoArray['persistent_variable'],
+        $tpl->variable('persistent_variable')
+    );
+}
+$Result['content_info'] = $contentInfoArray;
